c20de37e172378a8d936c93828e4dc15254665a5
Manuchekhr Rakhimov <30647642+ManuRakh@users.noreply.github.com>
2024-01-09 13:55:00 +0300

    Fixed webpack and dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Точка входа вашего приложения
+  entry: './src/index.js', // Entry point of your application
   output: {
-    path: path.resolve(__dirname, 'dist'), // Путь к директории для сборки
-    publicPath: '/', // Публичный URL-адрес для роутинга на стороне клиента
-    filename: 'bundle.js' // Имя файла сборки
+    path: path.resolve(__dirname, 'dist'), // Output directory path
+    publicPath: '/', // Public URL path for client-side routing
+    filename: 'bundle.js', // Output bundle filename
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'), // Директория для статических файлов
+      directory: path.join(__dirname, 'dist'), // Directory for static files
     },
-    port: 8080, // Порт для dev server
-    historyApiFallback: true, // Для поддержки HTML5 History API
-    open: true, // Открыть браузер после запуска сервера
+    port: 8080, // Dev server port
+    historyApiFallback: true, // Enable HTML5 History API support
+    open: true, // Open the browser after server start
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html', // Путь к вашему шаблону index.html
-      filename: 'index.html', // Имя файла, которое будет создано в dist
+      template: './index.html', // Path to your index.html template
+      filename: 'index.html', // Output filename in the 'dist' directory
     }),
   ],
   module: {
@@ -28,20 +28,30 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
-      // Добавьте другие правила если необходимо
-    ]
+      // Add other rules if needed
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'], // Расширения, которые будут обрабатываться
+    extensions: ['.js', '.jsx'], // File extensions to process
+  },
+  resolve: {
+    fallback: {
+      path: require.resolve('path-browserify'), // Polyfill for 'path' module
+      os: require.resolve('os-browserify/browser'), // Polyfill for 'os' module
+      stream: require.resolve('stream-browserify'), // Polyfill for 'stream' module
+      buffer: require.resolve('buffer'), // Polyfill for 'buffer' module
+      crypto: require.resolve('crypto-browserify'), // Polyfill for 'crypto' module
+
+    },
   },
 };
